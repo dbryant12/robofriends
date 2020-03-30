7a03c575daf5286191b15a1c21191ad29aa503e8
DB <DB@Davids-MacBook-Pro.local>
2020-03-30 00:00:47 -0400

    adding starting project

---
+++ build/precache-manifest.a3d292418bbd371b6d2df6fa6f830d58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0221addb5cd64e00b730de7926bd1b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c2059c77f884e8a3ed25",
+    "url": "/static/css/main.9e88d852.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2059c77f884e8a3ed25",
+    "url": "/static/js/main.c5d7d5ff.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
